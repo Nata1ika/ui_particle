b4578b118f850c7760f524e37464d34757e37315
Natalia Guliaeva <n_a_t_a_l_i_a_@mail.ru>
2018-09-10 03:01:59 +0300

    эффекты для слайдеров, движение джойстиков

---
+++ ui_particle_unity3d/Assets/Scr/JoysticController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class JoysticController : MonoBehaviour, IDragHandler, IPointerUpHandler, IPointerDownHandler
+{
+    public RectTransform _background;
+    public RectTransform _handle;
+
+    Vector2 _joystickCenter;
+    PointerEventData _eventData;
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        if (_eventData.pointerId != eventData.pointerId)
+        {
+            return;
+        }
+
+        var radius = _background.sizeDelta.x / 2f - _handle.sizeDelta.x * 0.25f;
+        var direction = eventData.position - _joystickCenter;
+        var inputVector = (direction.magnitude > radius)
+            ? direction.normalized
+            : direction / (radius);
+        _handle.anchoredPosition = inputVector * radius;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        if (_eventData.pointerId == eventData.pointerId)
+        {
+            _eventData = null;
+            _handle.position = _joystickCenter;
+        }
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        _eventData = eventData;
+        OnDrag(eventData);
+    }
+
+
+    void Start()
+    {
+        _joystickCenter = _handle.position;
+    }
+}
---
+++ ui_particle_unity3d/Assets/Scr/SliderEffect.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderEffect : MonoBehaviour
+{
+    [SerializeField] Slider _slider;
+
+    [SerializeField] ParticleSystem _up;
+    [SerializeField] ScreenWorldPosition _upPos;
+
+    [SerializeField] ParticleSystem _down;
+    [SerializeField] ScreenWorldPosition _downPos;
+
+    [SerializeField] Text _text;
+    const int MAX = 1000;
+    const float MAX_TIME = 3f;
+
+    float _value;
+    float _timeUp = -1f;
+    float _timeDown = -1f;
+
+    private void Start()
+    {
+        _value = _slider.value;
+        int value = (int)(_value * MAX);
+        _text.text = value.ToString();
+    }
+
+    public void OnValueChanged()
+    {
+        int value = (int)(_value * MAX);
+        _text.text = value.ToString();
+
+        if (_slider.value > _value)
+        {
+            _upPos.UpdatePosition();
+            _timeUp = MAX_TIME;            
+            _up.Play(true);
+
+            _timeDown = -1f;
+            _down.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
+        }
+        else
+        {
+            _downPos.UpdatePosition();            
+            _timeDown = MAX_TIME;
+            _down.Play(true);
+
+            _timeUp = -1f;
+            _up.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
+        }
+        _value = _slider.value;
+    }
+
+    private void Update()
+    {
+        if (_timeUp > 0)
+        {
+            _timeUp -= Time.deltaTime;
+            if (_timeUp <= 0)
+            {
+                _up.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
+            }
+        }
+
+        if (_timeDown > 0)
+        {
+            _timeDown -= Time.deltaTime;
+            if (_timeDown <= 0)
+            {
+                _down.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
+            }
+        }
+    }
+}
